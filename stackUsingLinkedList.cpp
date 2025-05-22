e7fbfe47f534d590a0023642d3a81cb89aab408a
Chintya-aa <tyachintyachin@gmail.com>
2025-05-22 10:00:49 +0700

    stuck class

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -13,3 +13,26 @@ class Node {
             next = NULL;
         }
 }; 
+
+//stuck class
+class Stack
+{
+    private:
+        Node* top; // pointer to the top node of the stack
+
+    public:
+        Stack()
+        {
+            top = NULL; // Initialize the stack with a null top pointer
+        }
+
+// push operation: insert an element onto the top of the stack
+        int push(int value)
+        {
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value: " << value << endl;
+            return value;
+        }
