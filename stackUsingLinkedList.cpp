4adbff3d3fd0865df2f89889420c639b81302c3e
Chintya-aa <tyachintyachin@gmail.com>
2025-05-22 10:07:56 +0700

    remove the topmost

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -2,37 +2,52 @@
 
 using namespace std;
 
-//node class representing a single node in the linked list
-class Node {
-    public:
-        int data;
-        Node* next;
+// node class representing a single node in the linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
 
-        Node()
-        {
-            next = NULL;
-        }
-}; 
+    Node()
+    {
+        next = NULL;
+    }
+};
 
-//stuck class
+// stuck class
 class Stack
 {
-    private:
-        Node* top; // pointer to the top node of the stack
+private:
+    Node *top; // pointer to the top node of the stack
 
-    public:
-        Stack()
+public:
+    Stack()
+    {
+        top = NULL; // Initialize the stack with a null top pointer
+    }
+
+    // push operation: insert an element onto the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
+
+    // pop operation: Remove  the topmost element from the stack
+    void pop()
+    {
+        if (isEmpty())
         {
-            top = NULL; // Initialize the stack with a null top pointer
+            cout << "Stack is Empty" << endl;
         }
 
-// push operation: insert an element onto the top of the stack
-        int push(int value)
-        {
-            Node *newNode = new Node();
-            newNode->data = value;
-            newNode->next = top;
-            top = newNode;
-            cout << "Push Value: " << value << endl;
-            return value;
-        }
+        Node *temp = top; // Create a temporary pointer to the top node
+        top = top->next;  // update the top pointer to the next node
+        cout << "Popped Value: " << top->data << endl;
+    }
+    
